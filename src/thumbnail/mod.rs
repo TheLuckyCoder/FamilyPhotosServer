db2877a9b3bfc3a2644f73abbe795835fdab334e
Razvan Filea <razvan.filea@gmail.com>
2023-09-09 11:01:41 +0300

    Migrate the generate_all_background function to the ThumbnailManager

--- src/thumbnail/mod.rs
+++ src/thumbnail/mod.rs
@@ -1,11 +1,11 @@
 use rayon::prelude::*;
+use tokio::task;
 
 use generate::*;
 pub use manager::*;
 
 use crate::http::AppState;
 use crate::model::photo::{Photo, PhotoBase};
-use crate::utils::file_storage::FileStorage;
 
 mod generate;
 mod manager;
@@ -18,15 +18,20 @@ pub async fn generate_all_foreground(app_state: &AppState) -> Result<(), String>
         .map_err(|_| "Could not load photos".to_string())?;
 
     photos.par_iter().for_each(|photo| {
-        create_thumbnail(&app_state.storage, photo);
+        let photo_path = app_state.storage.resolve_photo(photo.partial_path());
+        let thumbnail_path = app_state
+            .storage
+            .resolve_thumbnail(photo.partial_thumbnail_path());
+
+        if photo_path.exists() && !thumbnail_path.exists() {
+            generate_thumbnail(photo_path, thumbnail_path);
+        }
     });
 
     Ok(())
 }
 
 pub async fn generate_all_background(app_state: AppState) -> Result<(), String> {
-    let storage = app_state.storage;
-
     let photos: Vec<Photo> = app_state
         .photos_repo
         .get_photos()
@@ -34,20 +39,21 @@ pub async fn generate_all_background(app_state: AppState) -> Result<(), String> 
         .map_err(|_| "Could not load photos".to_string())?;
 
     // We only use one thread for this as we don't want to take up the whole CPU
-    rayon::spawn(move || {
+    task::spawn(async move {
         for photo in photos {
-            create_thumbnail(&storage, &photo);
+            let photo_path = app_state.storage.resolve_photo(photo.partial_path());
+            let thumbnail_path = app_state
+                .storage
+                .resolve_thumbnail(photo.partial_thumbnail_path());
+
+            if photo_path.exists() {
+                app_state
+                    .thumbnail_manager
+                    .request_thumbnail(photo.id, photo_path, thumbnail_path)
+                    .await;
+            }
         }
     });
 
     Ok(())
-}
-
-fn create_thumbnail(storage: &FileStorage, photo: &Photo) {
-    let photo_path = storage.resolve_photo(photo.partial_path());
-    let thumbnail_path = storage.resolve_thumbnail(photo.partial_thumbnail_path());
-
-    if photo_path.exists() && !thumbnail_path.exists() {
-        generate_thumbnail(photo_path, thumbnail_path);
-    }
 }
