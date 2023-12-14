b3ab51b449192226fbbd09ceb8751dfaf8f67181
Razvan Filea <razvan.filea@gmail.com>
2023-12-15 00:32:30 +0200

    Fix the convert command

--- src/previews/generate.rs
+++ src/previews/generate.rs
@@ -1,38 +1,13 @@
-use std::cmp::max;
-use std::fs::File;
-use std::io::{BufReader, BufWriter};
 use std::path::Path;
 use std::process::Command;
 use std::time::Duration;
 
 use anyhow::Context;
-use exif::{In, Tag};
-use image::codecs::jpeg::JpegEncoder;
-use image::imageops::FilterType;
-use image::DynamicImage;
 use mime_guess::MimeGuess;
-use tracing::{error, info, warn};
+use tracing::{info, warn};
 use wait_timeout::ChildExt;
 
 const PREVIEW_TARGET_SIZE: u32 = 400;
-
-fn generate_heif_preview(load_path: &Path, save_path: &Path) -> anyhow::Result<bool> {
-    let mut child = Command::new("heif-thumbnailer")
-        .arg("-s")
-        .arg(stringify!(PREVIEW_TARGET_SIZE))
-        .arg(load_path)
-        .arg(save_path)
-        .spawn()
-        .context("Failed to start heif-thumbnailer command")?;
-
-    match child.wait_timeout(Duration::from_secs(5)) {
-        Ok(status) => Ok(status.map_or(false, |s| s.success())),
-        Err(e) => {
-            child.kill()?;
-            Err(e).context("Error while rung heif-thumbnailer")
-        }
-    }
-}
 
 fn generate_video_frame<P: AsRef<Path>, R: AsRef<Path>>(
     load_path: P,
@@ -88,9 +63,8 @@ where
     let mut child = Command::new("convert")
         .arg("-auto-orient")
         .arg(load_path.as_ref())
-        .arg(format!(
-            "-thumbnail '{PREVIEW_TARGET_SIZE}x{PREVIEW_TARGET_SIZE}^'"
-        ))
+        .arg("-thumbnail")
+        .arg(format!("{PREVIEW_TARGET_SIZE}x{PREVIEW_TARGET_SIZE}^"))
         .arg(save_path.as_ref())
         .spawn()
         .unwrap();
@@ -105,75 +79,4 @@ where
     .unwrap();
 
     true
-}
-
-fn read_exif_orientation(path: &Path) -> Option<u32> {
-    let mime = MimeGuess::from_ext(path.extension()?.to_str()?).first_or_octet_stream();
-    if mime.type_() != "image" {
-        return None;
-    }
-
-    let file = File::open(path).ok()?;
-    let mut bufreader = BufReader::new(&file);
-    let reader = exif::Reader::new()
-        .read_from_container(&mut bufreader)
-        .ok()?;
-
-    if let Some(orientation) = reader.get_field(Tag::Orientation, In::PRIMARY) {
-        return orientation.value.get_uint(0);
-    }
-
-    // convert PXL_20231130_184007984.jpg -thumbnail '400x400^' thumbnail.jpg
-
-    None
-}
-
-fn resize_and_save_image<R>(save_path: R, img: DynamicImage, orientation: Option<u32>) -> bool
-where
-    R: AsRef<Path>,
-{
-    let (width, height) = resize_dimensions_fill(img.width(), img.height());
-
-    let mut preview = img.resize_exact(width, height, FilterType::Nearest);
-    match orientation {
-        Some(3) => preview = preview.rotate180(),
-        Some(6) => preview = preview.rotate90(),
-        Some(8) => preview = preview.rotate270(),
-        _ => {}
-    };
-
-    match save_path.as_ref().to_string_lossy().rsplit_once('.') {
-        None => false,
-        Some((_before, after)) => {
-            if after == "jpg" || after == "jpeg" {
-                let file = File::create(save_path).unwrap();
-                let writer = BufWriter::new(file);
-                JpegEncoder::new_with_quality(writer, 70)
-                    .encode_image(&preview)
-                    .is_ok()
-            } else {
-                preview.save(save_path).is_ok()
-            }
-        }
-    }
-}
-
-#[inline]
-fn resize_dimensions_fill(width: u32, height: u32) -> (u32, u32) {
-    let wratio = PREVIEW_TARGET_SIZE as f64 / width as f64;
-    let hratio = PREVIEW_TARGET_SIZE as f64 / height as f64;
-    let ratio = f64::max(wratio, hratio);
-
-    let nw = max((width as f64 * ratio).round() as u64, 1);
-    let nh = max((height as f64 * ratio).round() as u64, 1);
-
-    if nw > u64::from(u32::MAX) {
-        let ratio = u32::MAX as f64 / width as f64;
-        (u32::MAX, max((height as f64 * ratio).round() as u32, 1))
-    } else if nh > u64::from(u32::MAX) {
-        let ratio = u32::MAX as f64 / height as f64;
-        (max((width as f64 * ratio).round() as u32, 1), u32::MAX)
-    } else {
-        (nw as u32, nh as u32)
-    }
 }
